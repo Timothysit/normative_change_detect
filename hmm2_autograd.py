e7059ea420232cc6acc13b55fcdbf1a3c00563d4
timothy.sit <sitpakhang@gmail.com>
2019-04-22 11:18:57 +0100

    First working version of sequence gradient descent

--- hmm2_autograd.py
+++ hmm2_autograd.py
@@ -41,7 +41,7 @@ def cal_p_x_given_z2(x_k):
 
 def cal_p_x_given_z(x_k):
     z_mu = np.log(np.array([1.0, 1.25, 1.35, 1.50, 2.00, 4.00]))
-    z_var = np.array([0.25, 0.25, 0.25, 0.25, 0.25, 0.25])
+    z_var = np.array([0.25, 0.25, 0.25, 0.25, 0.25, 0.25]) ** 2 # in actual data, std is 0.25
 
     # z_mu = np.log(np.array([1.0, 1.25]))
     # z_var = np.array([0.25, 0.25])
@@ -101,7 +101,7 @@ def forward_inference(x):
     p_z1_given_x = list()
     p_z2_given_x = list()
 
-    hazard_rate = 0.00001
+    hazard_rate = 0.0001
 
     transition_matrix = np.array([[1 - hazard_rate, hazard_rate/5.0, hazard_rate/5.0, hazard_rate/5.0, hazard_rate/5.0, hazard_rate/5.0],
                                   [0, 1, 0, 0, 0, 0],
@@ -124,6 +124,9 @@ def forward_inference(x):
 
     # Initial probabilities
     p_z_and_x = cal_p_x_given_z(x_k=x[0]) * init_state_probability
+
+    # add the initial probability to the output list
+    p_change_given_x.append(0)
 
 
     # Loop through the rest of the samples to compute P(x_k, z_k) for each
@@ -165,13 +168,16 @@ def apply_cost_benefit(change_posterior, true_positive=1.0, true_negative=1.0, f
     return prob_lick
 
 
-def apply_strategy(prob_lick, k, midpoint=0.5):
+def apply_strategy(prob_lick, k, midpoint=0.5, baseline=0):
     # four param logistic function
     # prob_lick = y = a * 1 / (1 + np.exp(-k * (x - x0))) + b
 
+    # baseline offset
+    # avoiding outputting zero probability (will make cross entropy impossible to calculate)
+
     # two param logistic function
-    max_val = 1  # L
-    p_lick = max_val / (1 + np.exp(-k * (prob_lick - midpoint)))
+    max_val = 1 - baseline  # L
+    p_lick = max_val / (1 + np.exp(-k * (prob_lick - midpoint))) + baseline
 
     return p_lick
 
@@ -202,7 +208,7 @@ def loss_function(param_vals):
     cross_entropy_loss = 0
     for x_num, x in enumerate(signals):
         posterior = forward_inference(x.flatten())
-        p_lick = apply_cost_benefit(change_posterior=posterior[:, 1], true_positive=1.0, true_negative=0.0,
+        p_lick = apply_cost_benefit(change_posterior=posterior, true_positive=1.0, true_negative=0.0,
                                     false_negative=0.0, false_positive=0.0)
         p_lick = apply_strategy(p_lick, k=param_vals[0])
         p_lick = np.max(p_lick)
@@ -228,7 +234,7 @@ def loss_function_iter(param_vals, iter):
     cross_entropy_loss = 0
     for x_num, x in enumerate(signals):
         posterior = forward_inference(x.flatten())
-        p_lick = apply_cost_benefit(change_posterior=posterior[:, 1], true_positive=1.0, true_negative=0.0,
+        p_lick = apply_cost_benefit(change_posterior=posterior, true_positive=1.0, true_negative=0.0,
                                     false_negative=param_vals[1], false_positive=0.0)
         p_lick = apply_strategy(p_lick, k=param_vals[0], midpoint=param_vals[2])
         p_lick = np.max(p_lick)
@@ -256,7 +262,7 @@ def plot_signal_and_inference(signal, tau, prob, savepath=None):
     fig, axs = plt.subplots(2, 1, figsize=(8, 6))
 
     axs[0].plot(signal)
-    axs[0].axvline(tau, color="r", linestyle="--")
+    axs[0].axvline(tau, color="r", linestyle="--", linewidth=2.0)
 
     axs[1].plot(prob)
 
@@ -266,38 +272,41 @@ def plot_signal_and_inference(signal, tau, prob, savepath=None):
     plt.show()
 
 
-def test_on_data(change_val=1.0):
-    exp_data_file_list = ["/media/timothysit/180C-2DDD/second_rotation_project/exp_data/data/data_IO_083.mat"]
-    for exp_data_file in exp_data_file_list:
-        exp_data = scipy.io.loadmat(exp_data_file)
+def test_on_data(exp_data_file, change_val=1.0):
+
+    with open(exp_data_file, "rb") as handle:
+        exp_data = pkl.load(handle)
 
     trial_type_list = exp_data["hazard"]
     change_magnitude = np.exp(exp_data["sig"].flatten())
-    noiseless_trial_type = exp_data["noiseless"].flatten()
-    mouse_abort = (exp_data["outcome"].flatten() == "abort").astype(float)
+    # noiseless_trial_type = exp_data["noiseless"].flatten()
+    # mouse_abort = (exp_data["outcome"].flatten() == "abort").astype(float)
 
     # remove aborted trials, and noisless trials
     if change_magnitude is not None:
-        trial_index = np.where((mouse_abort == 0) & (noiseless_trial_type == 0) * (change_magnitude == change_val))[0]
+        # trial_index = np.where((mouse_abort == 0) & (noiseless_trial_type == 0) * (change_magnitude == change_val))[0]
+        trial_index = np.where(change_magnitude == change_val)[0]
     else:
         trial_index = np.where((mouse_abort == 0) & (noiseless_trial_type == 0))[0]
 
     signal = exp_data["ys"].flatten()[trial_index][0][0]
     tau = exp_data["change"][trial_index][0][0]
 
+    # print("Signal baseline mean:", )
+
     p_z_given_x = forward_inference(signal)
 
     # Plot example
-    plot_signal_and_inference(signal=signal, tau=tau, prob=p_z_given_x[:, 1])
+    plot_signal_and_inference(signal=signal, tau=tau, prob=p_z_given_x)
 
     return None
 
 
-def posterior_to_decision(posterior, return_prob=True, k=1.0, false_negative=0.0):
-    change_posterior = posterior[:, 1]  # p(z_2 | x_{1:k})
+def posterior_to_decision(posterior, return_prob=True, k=1.0, false_negative=0.0, midpoint=0.5):
+    change_posterior = posterior  # p(z_2 | x_{1:k})
     p_lick = apply_cost_benefit(change_posterior, true_positive=1.0, true_negative=0.0,
                                 false_negative=false_negative, false_positive=0.0)
-    p_lick = apply_strategy(p_lick, k=k)
+    p_lick = apply_strategy(p_lick, k=k, midpoint=midpoint)
 
     if return_prob is True:
         return p_lick
@@ -313,6 +322,8 @@ def run_through_entire_dataset(datapath, savepath, param, numtrial=100):
     signals = exp_data["ys"].flatten()
     change = np.exp(exp_data["sig"].flatten())
     tau = exp_data["change"].flatten()
+    absolute_decision_time = exp_data["rt"].flatten()
+    peri_stimulus_rt = absolute_decision_time - tau
 
     # Experimental data
     mouse_hit = (exp_data["outcome"] == "Hit").astype(float).flatten()
@@ -336,7 +347,8 @@ def run_through_entire_dataset(datapath, savepath, param, numtrial=100):
 
         posterior = forward_inference(signal.flatten())
 
-        lick_choice = posterior_to_decision(posterior, return_prob=True, k=param[0], false_negative=param[1])
+        lick_choice = posterior_to_decision(posterior, return_prob=True, k=param[0], false_negative=param[1],
+                                            midpoint=param[2])
         lick_choice = np.max(lick_choice)
 
         # save data
@@ -362,9 +374,10 @@ def run_through_entire_dataset(datapath, savepath, param, numtrial=100):
     # save the behaviour
     model_behaviour = dict()
     model_behaviour["change"] = change[0:numtrial]
-    model_behaviour["decision"] = decision
+    model_behaviour["decision"] = decision[0:numtrial]
     model_behaviour["tau"] = tau[0:numtrial]
     model_behaviour["mouse_lick"] = mouse_lick[0:numtrial]
+    model_behaviour["mouse_decision_time"] = absolute_decision_time[0:numtrial]
 
     # model_behaviour["lick_choice_list"] = lick_choice_list
 
@@ -375,7 +388,13 @@ def run_through_entire_dataset(datapath, savepath, param, numtrial=100):
         pkl.dump(model_behaviour_df, handle)
 
 
-def simple_gradient_descent(datapath, training_savepath, num_epoch=100):
+def simple_gradient_descent(datapath, training_savepath, param_vals=np.array([10.0, 0.0, 0.5]), num_epoch=100):
+    """
+    parameters:
+    0: k (curve of the sigmoid function)
+    1: false_negative
+    2: midpoint (sigmoid function)
+    """
     # Define targets (actual licks)
     # exp_data_file = "/media/timothysit/180C-2DDD/second_rotation_project/exp_data/data/data_IO_083.mat"
     # exp_data = scipy.io.loadmat(exp_data_file)
@@ -411,20 +430,13 @@ def simple_gradient_descent(datapath, training_savepath, num_epoch=100):
 
     # Simple Gradient Descent
     loss_function_grad = grad(loss_function)
-    param_vals = np.array([10.0, 0.0, 0.5])  # original: 1.0, 0.5, 0.5
-    """
-    parameters: 
-    0: k (curve of the sigmoid function)
-    1: false_negative
-    2: midpoint (sigmoid function)
-    """
 
     learning_rate = 0.01
 
     # print("Initial gradient:", loss_function_grad(param_vals))
 
     loss_val_list = list()
-    num_param = 1
+    num_param = len(param_vals)
     param_val_array = np.zeros((num_epoch, num_param))
 
     # simple gradient descent:
@@ -461,6 +473,132 @@ def simple_gradient_descent(datapath, training_savepath, num_epoch=100):
         pkl.dump(training_result, handle)
 
 
+def gradient_descent_fit_vector(exp_data_path, training_savepath, init_param_vals=np.array([10.0, 0.0, 0.5]),
+                                time_shift_list=np.arange(0, 5), num_epoch=10):
+    """
+
+    :return:
+    """
+
+
+    with open(exp_data_path, "rb") as handle:
+        exp_data = pkl.load(handle)
+
+    reaction_time = exp_data["rt"]
+    mouse_hit = (exp_data["outcome"] == "Hit").astype(float).flatten()
+    mouse_FA = (exp_data["outcome"] == "FA").astype(float).flatten()
+    mouse_lick = np.any([mouse_hit, mouse_FA], axis=0).astype(float).flatten()
+    mouse_rt = exp_data["rt"].flatten()
+    signal = exp_data["ys"].flatten()
+
+    mouse_lick_vector_list = list()
+    for trial in np.arange(0, len(mouse_rt)):
+        if not np.isnan(mouse_rt[trial]):
+            mouse_lick_vector = np.zeros(shape=(int(mouse_rt[trial]), ))
+            mouse_lick_vector[int(mouse_rt[trial] - 1)] = 1
+            mouse_lick_vector_list.append(mouse_lick_vector)
+        else:
+            mouse_lick_vector = np.zeros(shape=(len(signal[trial][0])), )
+            mouse_lick_vector_list.append(mouse_lick_vector)
+
+    assert len(signal) == len(mouse_lick_vector_list) # check equal number of trials
+
+    signal = exp_data["ys"].flatten()
+
+    # Define global variables used by loss_function
+    global actual_lick_vector
+    global signals
+    actual_lick_vector = mouse_lick_vector_list
+    signals = signal
+
+    global time_shift
+
+    loss_val_matrix = np.zeros((num_epoch, len(time_shift_list)))
+    final_param_list = list() # list of list, each list is the parameter values for a particular time shift
+
+
+    # callback function to get loss and parametervalues
+    def callback(weights, iter, gradient):
+        if iter % 5 == 0:
+            train_loss = loss_function_fit_vector(weights, 0)
+            print("Iteration", iter, "Train loss:", train_loss)
+            print("Gradient", gradient)
+            loss_val_list.append(train_loss)
+
+    # print("testing loss function")
+    for time_shift in time_shift_list:
+        param_vals = init_param_vals
+        loss_function_fit_vector(param_vals, iter=0)
+
+
+    for time_shift in time_shift_list:
+        param_vals = init_param_vals # reset initial parameter values for each gradient descent.
+        loss_val_list = list()
+        # ADAM Optimisation
+        loss_function_grad = grad(loss_function_fit_vector)
+                # TODO: Save the weights as well.
+
+        trained_params = adam(loss_function_grad, param_vals, step_size=0.01,
+                              num_iters=num_epoch, callback=callback)
+
+        final_param_list.append(trained_params)
+
+    training_result = dict()
+    training_result["loss"] = loss_val_matrix
+    training_result["param_val"] = final_param_list
+
+    with open(training_savepath, "wb") as handle:
+        pkl.dump(training_result, handle)
+
+
+def loss_function_fit_vector(param_vals, iter):
+    # missing arguments: signals, param_vals_, actual_lick_,
+    """
+    loss between predicted licks and actual licks
+    computed using cross entropy
+    This loss function is for licking versus no-licking, but does not compute the loss of the reaction time.
+    :param: param_vals, array of parameter values, in the following order:
+        1. k parameter in the sigmoid function
+        2. false_negative value in cost-benefit function
+        3. midpoint of the sigmoid decision function
+    :return:
+    """
+
+    cross_entropy_loss = 0
+    for x_num, x in enumerate(signals):
+        posterior = forward_inference(x.flatten())
+
+        p_lick = apply_cost_benefit(change_posterior=posterior, true_positive=1.0, true_negative=0.0,
+                                    false_negative=param_vals[1], false_positive=0.0)
+        p_lick = apply_strategy(p_lick, k=param_vals[0], midpoint=param_vals[2])
+        # p_lick = np.max(p_lick) # this is the main difference between this function and loss_functinon
+
+        # apply time shift to the posterior, relies time_shift global var
+        # pad on the left (make model slower)
+
+        # p_lick = np.pad(p_lick, (time_shift, 0), "constant", constant_values=(0, ))
+        # padding doesn't work with some versions of autograd
+        # p_lick = np.concatenate([np.zeros(time_shift), p_lick])
+        baseline = 0.01
+        p_lick = np.concatenate([np.repeat(baseline, time_shift), p_lick])
+        p_lick = p_lick[0:len(actual_lick_vector[x_num])]  # clip p(lick) to be same length as actual lick
+
+
+        # Caution: if k is quite lager, then the sigmoid may output 0, and np.log() will return an error.
+
+        # TODO: pad on the right (make model faster) (actually just take window)
+        cross_entropy_loss += np.sum(-(actual_lick_vector[x_num] * np.log(p_lick) + (1 - actual_lick_vector[x_num]) * np.log(
+            1 - p_lick)))  # sum over the entire time series
+
+        # print(x_num, cross_entropy_loss)
+
+        # TODO: likely have to be weighted cross-entropy
+
+    return cross_entropy_loss
+
+
+
+
 def plot_training_result(training_savepath):
     with open(training_savepath, "rb") as handle:
         training_result = pkl.load(handle)
@@ -489,6 +627,8 @@ def compare_model_with_behaviour(model_behaviour_df_path, savepath=None, showfig
 
     fig, ax = plt.subplots(figsize=(8, 6))
 
+    # TODO: Add reaction time to the comparison
+
     # Plot model behaviour
     ax.plot(model_prop_choice.change, model_prop_choice.decision)
     ax.scatter(model_prop_choice.change, model_prop_choice.decision)
@@ -516,9 +656,9 @@ def compare_model_with_behaviour(model_behaviour_df_path, savepath=None, showfig
 
 def main():
     datapath = "/media/timothysit/180C-2DDD/second_rotation_project/exp_data/subsetted_data/data_IO_083.pkl"
-    model_save_path = "/media/timothysit/180C-2DDD/second_rotation_project/hmm_data/model_response_083_12.pkl"
-    fig_save_path = "/media/timothysit/180C-2DDD/second_rotation_project/figures/model_response_083_12.png"
-    training_savepath = "/media/timothysit/180C-2DDD/second_rotation_project/hmm_data/training_result_083_13.pkl"
+    model_save_path = "/media/timothysit/180C-2DDD/second_rotation_project/hmm_data/model_response_083_16.pkl"
+    fig_save_path = "/media/timothysit/180C-2DDD/second_rotation_project/figures/model_response_083_16.png"
+    training_savepath = "/media/timothysit/180C-2DDD/second_rotation_project/hmm_data/training_result_083_17.pkl"
 
     """
     param: 
@@ -528,11 +668,18 @@ def main():
     """
 
     # test_forward_algorithm()
-    # test_on_data(change_val=2.0)
-    simple_gradient_descent(datapath, num_epoch=50, training_savepath=training_savepath)
+    # test_on_data(change_val=1.35, exp_data_file=datapath)
+    # simple_gradient_descent(datapath, num_epoch=100, param_vals=np.array([9.59, 0.27, 0.37]),
+    #                         training_savepath=training_savepath)
+
+    gradient_descent_fit_vector(datapath, training_savepath, init_param_vals=np.array([10.0, 0.0, 0.5]),
+                                time_shift_list=np.arange(0, 5), num_epoch=10)
+
     # plot_training_result(training_savepath)
-    # run_through_entire_dataset(datapath=datapath, savepath=model_save_path, param=[1000, 0, 0.5], numtrial=None)
+    # run_through_entire_dataset(datapath=datapath, savepath=model_save_path, param=[9.23, 0.67, 0.03], numtrial=None)
     # compare_model_with_behaviour(model_behaviour_df_path=model_save_path, savepath=fig_save_path, showfig=True)
+
+
 
 
 if __name__ == "__main__":
