119a6183d8be76328c6108101d3e0daae23a4d7d
timothy.sit <sitpakhang@gmail.com>
2019-04-30 10:50:30 +0100

    keep only running trials

---
+++ process_exp_data.py
@@ -0,0 +1,65 @@
+import pickle as pkl
+import pandas as pd
+import scipy.io
+import glob
+import os
+import numpy as np
+
+
+"""
+Script to process exp data
+
+"""
+
+
+from os.path import expanduser
+
+def main():
+    home = expanduser("~")
+    mainfolder = os.path.join(home, "Dropbox/notes/Projects/second_rotation_project/normative_model")
+
+
+    exp_data_folder = os.path.join(mainfolder, "exp_data/data/")
+    processed_data_folder = os.path.join(mainfolder, "exp_data/subsetted_data/")
+
+    exp_file_names = [f for f in glob.glob(exp_data_folder + "**/*.mat", recursive=True)]
+
+    for exp_file_name in exp_file_names:
+        exp_data = scipy.io.loadmat(exp_file_name)
+
+        noiseless_trial_type = exp_data["noiseless"].flatten()
+        mouse_abort = (exp_data["outcome"].flatten() == "abort").astype(float)
+
+        # remove running trials
+        mouse_move = (exp_data["running"] == "running").flatten().astype(float)
+        # subset_trial_index = np.where( (noiseless_trial_type == 0) & (mouse_abort == 0) & (mouse_move == 0) )[0]
+
+        # remove stationary trials
+        subset_trial_index = np.where((noiseless_trial_type == 0) & (mouse_abort == 0) & (mouse_move == 1))[0]
+
+
+        # allow running trials
+        # subset_trial_index = np.where((noiseless_trial_type == 0) & (mouse_abort == 0))[0]
+
+
+        new_exp_data = dict()
+        keys_of_interest = ["ys", "rt", "change", "sig", "sig_avg", "sig_std", "noiseless", "hazard", "running", "outcome"]
+        exp_data["ys"] = exp_data["ys"].T
+        exp_data["rt"] = exp_data["rt"].T
+
+        for key_to_inquire in keys_of_interest:
+            new_exp_data[key_to_inquire] = exp_data[key_to_inquire][subset_trial_index]
+
+
+
+        # save new data in processed_data_folder
+        new_file_name = os.path.basename(exp_file_name)
+        new_file_name = new_file_name.replace(".mat", ".pkl")
+
+        with open(os.path.join(processed_data_folder, new_file_name), "wb") as handle:
+            pkl.dump(new_exp_data, handle)
+
+
+if __name__ == "__main__":
+    main()
+
