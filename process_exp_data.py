547744605765d1445e511fc2024e9af542f48391
timothy.sit <sitpakhang@gmail.com>
2019-06-14 17:29:20 +0100

    allow subsetting early vs. late blocks

--- process_exp_data.py
+++ process_exp_data.py
@@ -29,31 +29,37 @@ def main():
 
         noiseless_trial_type = exp_data["noiseless"].flatten()
         mouse_abort = (exp_data["outcome"].flatten() == "abort").astype(float)
+        nonsplit_block = (exp_data["hazard"].flatten() == "nonsplit").astype(float)
+        early_block = (exp_data["hazard"].flatten() == "early").astype(float)
+        late_block = (exp_data["hazard"].flatten() == "late").astype(float)
 
         # remove running trials
         mouse_move = (exp_data["running"] == "running").flatten().astype(float)
         # subset_trial_index = np.where( (noiseless_trial_type == 0) & (mouse_abort == 0) & (mouse_move == 0) )[0]
 
-        # remove stationary trials
-        subset_trial_index = np.where((noiseless_trial_type == 0) & (mouse_abort == 0) & (mouse_move == 1))[0]
+        # Subset criteria
+        subset_trial_index = np.where((noiseless_trial_type == 0) & (mouse_abort == 0) & (early_block == 1))[0]
 
 
         # allow running trials
         # subset_trial_index = np.where((noiseless_trial_type == 0) & (mouse_abort == 0))[0]
 
+        # subset based on block type (nonsplit, early, late)
 
         new_exp_data = dict()
         keys_of_interest = ["ys", "rt", "change", "sig", "sig_avg", "sig_std", "noiseless", "hazard", "running", "outcome"]
         exp_data["ys"] = exp_data["ys"].T
         exp_data["rt"] = exp_data["rt"].T
 
+        # TODO: include info about the subset crieteria in the file.
+        exp_data["subset_criteria"] = ["noisy", "no_abort", "early_block"]
+
+
         for key_to_inquire in keys_of_interest:
             new_exp_data[key_to_inquire] = exp_data[key_to_inquire][subset_trial_index]
 
-
-
         # save new data in processed_data_folder
-        new_file_name = os.path.basename(exp_file_name)
+        new_file_name = os.path.basename(exp_file_name + "_early_blocks")
         new_file_name = new_file_name.replace(".mat", ".pkl")
 
         with open(os.path.join(processed_data_folder, new_file_name), "wb") as handle:
